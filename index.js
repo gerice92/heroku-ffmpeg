69ccc1e355ccc698eabaa7f50111b7e8c838b766
Gerardo Quiros <gerardo.quiros@sngular.com>
2020-04-10 14:01:10 +0200

    removed express-rate-limit

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const utils = require('./utils.js')
 
 const PORT = process.env.PORT || 5000
 
-const rateLimit = require("express-rate-limit");
 const timeToTimeout = "5s"
  
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500 // limit each IP to 100 requests per windowMs
-});
- 
 express()
-  .use(limiter)
   .use(timeout(timeToTimeout))
   .get('/audio/:id', async function (req, res) {
     const id = req.params.id
